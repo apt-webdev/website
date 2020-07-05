1472e283f6c7900737c93fffc3232ddcf0fb1b7c
patricia <patriciareiasilva@gmail.com>
2020-07-05 23:43:54 +0100

    add narbar and about me text.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="about-text box-text">Hello,<br/> my name is <br/> <span className="name">Patrícia Areias Silva</span>. <br/>A Font End Developer <br/> and a dreamer.</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/components/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+// import { render } from '@testing-library/react';
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this. state= {
+            // comment
+            name: '', 
+            greeting: `good ${this.props.time}`
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <nav className='navbar' role='navigation' aria-label='main navigation'>
+                    <div className='navbar-brand'>
+                        <p className="brand-name">patriciareiasilva</p>
+                        <a
+                            role='button'
+                            className={'navbar-burger burger'}
+                            aria-label='menu'
+                            aria-expanded='false'
+                            data-target='navbarBasicExample'
+                        >
+                            <span aria-hidden='true'></span>
+                            <span aria-hidden='true'></span>
+                            <span aria-hidden='true'></span>
+                        </a>
+                    </div>
+                    <div id='navbarBasicExample' className='navbar-menu'>
+                        <div className='navbar-end'>
+                            <div className='navbar-item'>
+                                <a href='/' className='item navbar-item'>
+                                    About
+                                </a>
+                                <a href='/' className='item navbar-item'>
+                                    Skills
+                                </a>
+                                <a href='/' className='item navbar-item'>
+                                    Collabs
+                                </a>
+                                <a href='/' className='item navbar-item'>
+                                    Contact
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+
+
+                {/* <h2>I am a Car!</h2>
+                <section className="section">
+                    <label className="label">Name:</label>
+                    <input className="input" name="name" placeholder="Enter your name..." onChange={this.onChange} />
+                </section>
+                <section className="section">
+                    <p>{this.state.greeting} {this.state.name}</p>
+                </section> */}
+
+              
+            </div>
+        );
+    }
+}
+
+export default Navbar;
+// ReactDOM.render(<Greeting time="morning" />, document.getElementById('app'));
