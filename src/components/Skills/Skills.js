82589752ed4532b70329d939f8fc0bea74f8d8a1
patricia <patriciareiasilva@gmail.com>
2022-02-21 22:56:26 +0000

    update list of skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,60 +6,52 @@ import './Skills.scss';
 
 class Skills extends React.Component {
     render() {
-        return ( <
-            section className = "navbar-tab"
-            id = "my-skills" >
-            <
-            div className = "div-content" >
-            <
-            h1 className = "title " > Skills < /h1> <
-            /div> <
-            div className = "div-content" >
-            <
-            div className = "plain-text" >
-            During my academic path, I have gained a solid understanding of computer science, with a scientific and practical approach to computation and its applications. <
-            br / >
-            Recently, I have been dedicating my free time to the web development area. <
-            br / >
-            Some of the skills I master are listed below:
-            <
-            /div>
+        return (
+            <section className="navbar-tab" id="my-skills">
+                <div className="div-content">
+                    <h1 className="title ">Skills</h1>
+                </div>
+                <div className="div-content">
+                    <div className="plain-text">
+                        During my academic path, I have gained a solid understanding of computer science, with a scientific and practical approach to computation and its applications. 
+                        <br/>
+                        Recently, I have been dedicating my free time to the web development area. 
+                        <br/>
+                        Some of the skills I master are listed below:
+                    </div>
 
-            <
-            ul className = "skills plain-text" >
-            <
-            li >
-            JavaScript <
-            /li> <
-            li >
-            HTML5 and CSS3 <
-            /li> <
-            li >
-            React.js <
-            /li> <
-            li >
-            Vue.js <
-            /li> <
-            li >
-            Node.js <
-            /li> <
-            li >
-            Bulma and Bootstrap <
-            /li> <
-            li >
-            GraphQL <
-            /li> <
-            li >
-            Python and SQLite <
-            li >
-            Git, NPM <
-            /li> <
-            /li> <
-            /ul> <
-            /div>
+                    <ul className="skills plain-text">
+                        <li>
+                            JavaScript
+                        </li>
+                        <li>
+                            HTML5 and CSS3
+                        </li>
+                        <li>
+                            React.js
+                        </li>
+                        <li>
+                            Vue.js
+                        </li>
+                        <li>
+                            Node.js 
+                        </li>
+                        <li>
+                            Bulma and Bootstrap
+                        </li>
+                        <li>
+                            GraphQL 
+                        </li>
+                        <li>
+                            Python and SQLite
+                        <li>
+                            Git, NPM
+                        </li>
+                        </li>
+                    </ul>
+                </div>
 
-            <
-            /section>
+            </section>
         );
     }
 }
